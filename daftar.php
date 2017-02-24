29a2c6b77571864a8f772c8f769ce81fbe070902
raihan <rusun315@gmail.com>
2017-02-24 10:47:29 +0700

    CrudNatife

---
+++ daftar.php
@@ -0,0 +1,205 @@
+<?php
+session_start();
+if (isset($_SESSION['username'])) {
+  header('location: index.php');
+}
+?>
+<style>
+body {
+  font-family: "Open Sans", sans-serif;
+  height: 100vh;
+  background: url("http://imgur.com/a/n1W4f.jpg") 50% fixed;
+  background-size: cover;
+}
+
+@keyframes spinner {
+  0% {
+    transform: rotateZ(0deg);
+  }
+  100% {
+    transform: rotateZ(359deg);
+  }
+}
+* {
+  box-sizing: border-box;
+}
+
+.wrapper {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+  width: 100%;
+  min-height: 100%;
+  padding: 20px;
+  background: rgba(4, 40, 68, 0.85);
+}
+
+.daftar {
+  border-radius: 2px 2px 5px 5px;
+  padding: 10px 20px 20px 20px;
+  width: 90%;
+  max-width: 320px;
+  background: #ffffff;
+  position: relative;
+  padding-bottom: 80px;
+  box-shadow: 0px 1px 5px rgba(0, 0, 0, 0.3);
+}
+.daftar.loading button {
+  max-height: 100%;
+  padding-top: 50px;
+}
+.daftar.loading button .spinner {
+  opacity: 1;
+  top: 40%;
+}
+.daftar.ok button {
+  background-color: #8bc34a;
+}
+.daftar.ok button .spinner {
+  border-radius: 0;
+  border-top-color: transparent;
+  border-right-color: transparent;
+  height: 20px;
+  animation: none;
+  transform: rotateZ(-45deg);
+}
+.daftar input {
+  display: block;
+  padding: 15px 10px;
+  margin-bottom: 10px;
+  width: 100%;
+  border: 1px solid #ddd;
+  transition: border-width 0.2s ease;
+  border-radius: 2px;
+  color: #ccc;
+}
+.daftar input + i.fa {
+  color: #fff;
+  font-size: 1em;
+  position: absolute;
+  margin-top: -47px;
+  opacity: 0;
+  left: 0;
+  transition: all 0.1s ease-in;
+}
+.daftar input:focus {
+  outline: none;
+  color: #444;
+  border-color: #2196F3;
+  border-left-width: 35px;
+}
+.daftar input:focus + i.fa {
+  opacity: 1;
+  left: 30px;
+  transition: all 0.25s ease-out;
+}
+.daftar a {
+  font-size: 0.8em;
+  color: #2196F3;
+  text-decoration: none;
+}
+.daftar .title {
+  color: #444;
+  font-size: 1.2em;
+  font-weight: bold;
+  margin: 10px 0 30px 0;
+  border-bottom: 1px solid #eee;
+  padding-bottom: 20px;
+}
+.daftar button {
+  width: 100%;
+  height: 100%;
+  padding: 10px 10px;
+  background: #2196F3;
+  color: #fff;
+  display: block;
+  border: none;
+  margin-top: 20px;
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  max-height: 60px;
+  border: 0px solid rgba(0, 0, 0, 0.1);
+  border-radius: 0 0 2px 2px;
+  transform: rotateZ(0deg);
+  transition: all 0.1s ease-out;
+  border-bottom-width: 7px;
+}
+.daftar button .spinner {
+  display: block;
+  width: 40px;
+  height: 40px;
+  position: absolute;
+  border: 4px solid #ffffff;
+  border-top-color: rgba(255, 255, 255, 0.3);
+  border-radius: 100%;
+  left: 50%;
+  top: 0;
+  opacity: 0;
+  margin-left: -20px;
+  margin-top: -20px;
+  animation: spinner 0.6s infinite linear;
+  transition: top 0.3s 0.3s ease, opacity 0.3s 0.3s ease, border-radius 0.3s ease;
+  box-shadow: 0px 1px 0px rgba(0, 0, 0, 0.2);
+}
+.daftar:not(.loading) button:hover {
+  box-shadow: 0px 1px 3px #2196F3;
+}
+.daftar:not(.loading) button:focus {
+  border-bottom-width: 4px;
+}
+
+footer {
+  display: block;
+  padding-top: 50px;
+  text-align: center;
+  color: #ddd;
+  font-weight: normal;
+  text-shadow: 0px -1px 0px rgba(0, 0, 0, 0.2);
+  font-size: 0.8em;
+}
+footer a, footer a:link {
+  color: #fff;
+  text-decoration: none;
+}
+
+</style>
+<div class="wrapper">
+  <form class="daftar" action="prosesdaftar.php" method="post">
+    <p class="title">REGISTER</p>
+    <input type="text" placeholder="Username" name="username" autofocus/>
+    <i class="fa fa-user"></i>
+    <input type="password" name="password" placeholder="Password" />
+    <i class="fa fa-key"></i><br>
+    Already Account ? <a href="login.php">Sign In  !</a>
+    <button>
+      <i class="spinner"></i>
+      <span class="state">Register</span>
+    </button>
+  </form>
+  <footer><a target="blank" href="https://www.facebook.com/ekapzwashere">Facebook</a></footer>
+  </p>
+</div>
+<script>
+var working = false;
+$('.login').on('submit', function(e) {
+  e.preventDefault();
+  if (working) return;
+  working = true;
+  var $this = $(this),
+    $state = $this.find('button > .state');
+  $this.addClass('loading');
+  $state.html('Authenticating');
+  setTimeout(function() {
+    $this.addClass('ok');
+    $state.html('Welcome back!');
+    setTimeout(function() {
+      $state.html('Log in');
+      $this.removeClass('ok loading');
+      working = false;
+    }, 4000);
+  }, 3000);
+});
+</script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/prefixfree/1.0.7/prefixfree.min.js"></script>
---
+++ koneksi.php
@@ -0,0 +1,7 @@
+<?php
+	$hostname = "localhost";
+	$username = "root";
+	$password = "0218561432";
+	$dbname = "TUGAS";
+	$db = new mysqli($hostname, $username, $password, $dbname);
+?>
